9b1f4204f4a29522330b82c47e26702c505bedf0
Maggie Martin <maggierozay@gmail.com>
2014-10-27 18:29:37 -0400

    time to make gh pages work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,24 @@
-//set up my Main Object
 
-var maggie = {};
+var container=$('.image-grid'),
+item_title,
+item_store,
+item_price,
+display;
 
+  etsyList.results.forEach( function (object) {
 
+  item_title = "<h3>" + object.title + "</h3>";
 
-maggie.each = function (wassup){
-	console.log(wassup);
-};
+  item_store = "<p>" + object.Shop.shop_name; + "</p>";
+
+  item_price = "<span>" + object.price + " " + object.currency_code + "<span>";
+
+  object.Images.forEach (function (y) {
+  item_image = "<img src='" + y.url_170x135 + "' />";
+});
+
+  display = "<li>" + item_image + item_title + item_store + item_price + "</li>";
+  container.append(display);
+});
+
+//$('image-grid').find('li')
